87c3ecf433915e9c22df471770371455a05d4df6
LordOfTheCorgis <evanvoisel@yahoo.com>
2025-11-17 12:11:44 -0600

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'LordOfTheCorgis'
+description 'Simple Discord to In-Game Name Format Bridge'
+version '1.0.0'
+
+server_scripts {
+    'nameFormat/server.lua',
+}
